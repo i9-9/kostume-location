bd070a9afde626ccd0bb698d3a776871fc9a3d58
i9-9 <yvan.vrs@gmail.com>
2024-12-18 14:56:34 -0300

    videohero is clickable

--- app/components/VideoHero.tsx
+++ app/components/VideoHero.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Link from 'next/link';
 
 const VideoHero = () => {
   return (
     <div className="relative" style={{ paddingTop: '56.25%' }}>
+      <Link href="https://eshop.kostumeweb.net/ar/48ss25" className="absolute top-0 left-0 w-full h-full z-10" />
       <iframe 
         src="https://player.vimeo.com/video/1040442547?title=0&byline=0&portrait=0&controls=0&muted=1&autoplay=1&loop=1&quality=1080p"
         className="absolute top-0 left-0 w-full h-full" 
